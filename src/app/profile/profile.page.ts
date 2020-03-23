ca9bea6c254f97bed9f35c7402a596fb8ab9bb85
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-03-23 15:08:07 +0100

    Anzeige mit Viewmodel

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AlertController } from '@ionic/angular';
+import { Post } from 'src/viewmodel/Post';
 
 @Component({
   selector: 'app-profile',
@@ -9,14 +10,36 @@ import { AlertController } from '@ionic/angular';
 })
 export class ProfilePage implements OnInit {
 
-  myPost
+  // myPost
+
+  myPost: Post = new Post()
+  posts: Post[]
 
   constructor(public fireStore: AngularFirestore, public alertController: AlertController) { 
+    this.myPost.id = "42"
+    this.myPost.text = "Heikos Post"
+    this.myPost.userId = "0815"
+
+    /*
     this.fireStore.doc(`users/Kxov1VTzZlNBdqYPI97k6owVtAG3`)
-      .valueChanges()
+    .valueChanges()
+    .subscribe(data => {
+      this.myPost = data // data = {postText: "hallo finn", postTexts: "testtst"}
+   })
+    */
+
+    /*
+    this.fireStore.collection('posts')
+      .snapshotChanges()
       .subscribe(data => {
-        this.myPost = data // data = {postText: "hallo finn", postTexts: "testtst"}
-     })
+        this.posts = data.map(post => {
+          return {
+            id: post.payload.doc.id,
+            ...post.payload.doc.data()
+          } as Post;
+        });
+     });
+     */
   }
 
 
---
+++ src/viewmodel/Post.ts
@@ -0,0 +1,5 @@
+export class Post {
+    id: string;
+    userId: string;
+    text: string
+}
