1710c31ac84d4ab3e3ba5945db0172e014aa1bef
stolle.finn@googlemail.com <finn.stolle@live.de>
2020-03-29 14:28:03 +0200

    View Model for PublicPost created and binding

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -8,25 +8,18 @@ import { PublicPost } from 'src/viewmodel/PublicPost';
 })
 export class FeedPage implements OnInit {
 
-  publicPost: PublicPost = new PublicPost()
-  publicPosts: Array<PublicPost> = new Array
 
-  constructor() {
-    this.fillPost("42", "Heikos Post", "0815", "Hamburg", "12:01")
-    this.fillPost("43", "Heikos erwrPost", "0dsr815", "Bremen", "12:02")
-    this.fillPost("44", "Der bums läuft Junge!", "Florin", "Hamburg", "13:13")
+ 
+  publicPosts: Array<PublicPost> = [
+    new PublicPost("42", "Heikos Post", "0815", "Hamburg", "12:01"),
+    new PublicPost("43", "Heikos erwrPost", "0dsr815", "Bremen", "12:02"),
+    new PublicPost("44", "Der bums läuft Junge!", "Florin", "Hamburg", "13:13")
+  ]   
+
+  constructor() {   
+    
    }
 
   ngOnInit() {
-  }
-
-  fillPost(postId: string, text: string, userId: string, location: string, postTime: string) {
-    this.publicPost.postId = postId
-    this.publicPost.text = text
-    this.publicPost.userId = userId
-    this.publicPost.location = location
-    this.publicPost.postTime = postTime
-    this.publicPosts.push(this.publicPost)
-  }
-
+  }  
 }
--- src/viewmodel/PublicPost.ts
+++ src/viewmodel/PublicPost.ts
@@ -1,9 +1,19 @@
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe } from '@angular/common'; 
+
 
 export class PublicPost {
-    postId: string;
+    id: string;
     userId: string;
     text: string;
     location: string;
     postTime: string;
+
+    constructor(id: string,userId: string, text: string, location: string, postTime: string){
+        this.id = id,
+        this.userId = userId,
+        this.text = text,
+        this.location = location,
+        this.postTime = postTime
+      }
 }
+
